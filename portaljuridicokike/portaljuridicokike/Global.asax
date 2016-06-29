7cd9028572d8799f8a294ad38675b1fc0fb78778
Enrique Rojas Reyes <kinakeyoy@hotmail.com>
2016-06-29 12:06:35 -0500

    prueba desde kinakeyoy a ramadavid 1

---
+++ portaljuridicokike/portaljuridicokike/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="portaljuridicokike.MvcApplication" Language="C#" %>
---
+++ portaljuridicokike/portaljuridicokike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(portaljuridicokike.Startup))]
+namespace portaljuridicokike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
